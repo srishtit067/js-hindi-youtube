5411cf1dbcbc6dc6707191cc6b23a532e25419f8
srishtit067 <srishtit067@gmail.com>
2026-02-17 10:46:25 +0000

    part 2

---
+++ basics_02/02_arrays.js
@@ -0,0 +1,4 @@
+const marvel_heros =["thor","ironman","drstrange","hulk"]
+const dc =["superman","batman"]
+//concat and push methods
+// arrays ke andar arrays flat , array of etc etc hitesh -->> 'h','i',......
